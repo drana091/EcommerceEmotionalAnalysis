d5cac62836a487b41b37584f20fd521e8d84cc7d
Christopher Fallas <christopher.fallasaguero@gmail.com>
2024-03-23 21:10:02 -0400

    Added product images

--- emotionalAnalysis/api/models.py
+++ emotionalAnalysis/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 # Create your models here.
 
 class Product(models.Model):
@@ -7,10 +8,24 @@ class Product(models.Model):
     price = models.FloatField(default=0.0)
     stock = models.IntegerField(default = 0)
     totalEmotion = models.CharField(max_length=255, blank=True, null=True)
+    image_url = models.CharField(max_length=255, blank=True, null=True)
 
     def getReviews(self):
         return self.review_set.all()
+    
+    def generate_image_url(self):
+        # Replace spaces in product name with underscores and add .jpg extension
+        filename = f"{self.name.replace(' ', '_')}.jpg"
+        # Assuming images are stored in a folder named 'product_images' within MEDIA_URL
+        return os.path.join('media/productImages', filename)
 
+    def save(self, *args, **kwargs):
+        if not self.image_url:
+            # Generate image URL if not already set
+            self.image_url = self.generate_image_url()
+        super().save(*args, **kwargs)
+
+        
 class User(models.Model):
     Fname = models.CharField(max_length=255)
     Lname = models.CharField(max_length=255)
--- emotionalAnalysis/frontend/src/components/ProductBox.js
+++ emotionalAnalysis/frontend/src/components/ProductBox.js
@@ -1,45 +1,44 @@
 import { Button, Grid, Box, Typography } from '@mui/material'; 
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 // This component is used to display a product in a box.
-export default class ProductBox extends Component {
-    render() {
-        const { product } = this.props;
-        return (
+export default function ProductBox({ product }) {
+    // Construct the absolute URL of the image based on MEDIA_URL
 
-            // Box of product
-            <Box sx={{ border: '2px solid black' }}>
-                <Grid container spacing={1}>
+    return (
+        // Box of product
+        <Box sx={{ border: '2px solid black' }}>
+            <Grid container spacing={1}>
 
-                    {/* Product Image */}
+                {/* Product Image */}
+                <Grid item xs={12} sm={6}>
+                    {/* Use the absolute URL for the image */}
+                    <img src={product.image_url} alt={product.name} style={{ width: '100%' }} />
+                </Grid>
+
+                {/* Product Details */}
+                <Grid item xs={12} sm={6} container direction="column" spacing={1}>
+                    {/* Product Name, Description, Stock */}
                     <Grid item>
-                        <p>Image</p>
+                        <Typography variant="h6">{product.name}</Typography>
+                        <Typography>{product.description}</Typography>
+                        <Typography>Stock: {product.stock}</Typography>
                     </Grid>
-                    <Grid item xs={12} sm container>
-                        <Grid item xs container direction="column" spacing={1}>
 
-                            {/* Product Name, Description, Stock */}
-                            <Grid item xs>
-                                <Typography>{product.name}</Typography>
-                                <Typography>{product.description}</Typography>
-                                <Typography>{product.stock}</Typography>
-                            </Grid>
+                    {/* View Button */}
+                    <Grid item>
+                        <Button variant="contained" color="primary" component={Link} to={`/product/${product.id}`}>
+                            View
+                        </Button>
+                    </Grid>
 
-                            {/* View Button */}
-                            <Grid item>
-                                <Button variant="contained" color="primary"
-                                component={Link} to={`/product/${product.id}`}>View</Button>
-                            </Grid>
-                        </Grid>
-
-                        {/* Product Price */}
-                        <Grid item>
-                        <Typography>${product.price}</Typography>
-                        </Grid>
+                    {/* Product Price */}
+                    <Grid item>
+                        <Typography variant="h6">${product.price}</Typography>
                     </Grid>
                 </Grid>
-            </Box>
-        );
-    }
+            </Grid>
+        </Box>
+    );
 }
