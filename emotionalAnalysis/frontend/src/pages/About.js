1cbbeaebf97c2ab948d8796fbf0780628907755d
Christopher Fallas <christopher.fallasaguero@gmail.com>
2024-04-21 17:21:08 -0400

    Fix formatting in About.js and update license file

--- emotionalAnalysis/frontend/src/pages/About.js
+++ emotionalAnalysis/frontend/src/pages/About.js
@@ -109,7 +109,6 @@ export default function About() {
                     </React.Fragment>
                 ))}
                     
-                    
 
                 </Grid>
             </Grid>
